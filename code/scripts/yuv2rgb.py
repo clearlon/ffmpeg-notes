991e9bb2fd2b212eb8988cb3a51b5d2c4221fe88
clearlon <57201953+clearlon@users.noreply.github.com>
2021-09-27 15:05:40 +0800

    Update yuv2rgb.py

--- code/scripts/yuv2rgb.py
+++ code/scripts/yuv2rgb.py
@@ -2,15 +2,24 @@ import numpy as np
 import cv2
 import os
 import scipy.ndimage as ndimage
-from data.yuv_io import YUVread_16bit
+import glob
+import argparse
+from yuv_io import YUVread_16bit
 
 
-def yuv2rgb_2020(Dy, Du, Dv, yuv_bit_depth=10, quantification=16):
+def yuv2rgb_2020(Dy, Du, Dv, mode='420', yuv_bit_depth=10, quantification=16):
     """
-    input:Y,U,V
-    output:Drgb（uint16)
+    only for 420 or 444 yuv mode,default 420
+    input:Y,U,V（default 10bit）
+    output:Drgb（default uint16)
     """
-    #YUV去量化  针对10bit  若是16bit,则/256
+    if mode == '420':
+        #U,V分量resize成Y的shape，以便进行矩阵运算
+        Du = ndimage.zoom(Du, 2, order=0) #使用最近邻插值法
+        Dv = ndimage.zoom(Dv, 2, order=0)
+
+    h, w = Dy.shape
+    #YUV去量化
     Ey = ((Dy/2 ** (yuv_bit_depth - 8) - 16)/219).flatten()
     Eu = ((Du/2 ** (yuv_bit_depth - 8) - 128)/224).flatten()
     Ev = ((Dv/2 ** (yuv_bit_depth - 8) - 128)/224).flatten()
@@ -21,88 +30,65 @@ def yuv2rgb_2020(Dy, Du, Dv, yuv_bit_depth=10, quantification=16):
                             [0.5000, -0.4598, -0.0402]])
     yuv2rgb_2020_matrix = rgb2yuv_bt2020_matrix.I
     Ergb = np.dot(yuv2rgb_2020_matrix, Eyuv)
-    Er = Ergb[0,:].reshape(1920,1080)
-    Eg = Ergb[1,:].reshape(1920,1080)
-    Eb = Ergb[2,:].reshape(1920,1080)
-    #RGB量化   量化为16bit
+    Er = Ergb[0,:].reshape(h,w)
+    Eg = Ergb[1,:].reshape(h,w)
+    Eb = Ergb[2,:].reshape(h,w)
+    #RGB量化   
     Dr = np.round((219*Er+16)*2**(quantification - 8)).clip(0,65535)
     Dg = np.round((219*Eg+16)*2**(quantification - 8)).clip(0,65535)
     Db = np.round((219*Eb+16)*2**(quantification - 8)).clip(0,65535)
     a = [Dr,Dg,Db]
     Drgb = np.array([Dr,Dg,Db]).astype(np.uint16)
     Drgb = Drgb.transpose(1,2,0)  #(3,1920,1080)->(1920,1080,3)
-    # Drgb = Drgb.astype(np.uint16)
     return Drgb
 
-def yuv2rgb_709_matrix(Dy,Du,Dv):
-    """
-    input:Y,U,V
-    output:uint8 Drgb
-    """
-    h, w = Dy.shape
-    #YUV去量化  针对8bit
-    print(np.max(Dy))
-    Ey = ((Dy-16)/219).flatten()
-    print(np.max(Ey))
-    print(np.min(Ey))
-    Eu = ((Du-128)/224).flatten()
-    Ev = ((Dv-128)/224).flatten()
-    Eyuv = np.array([Ey,Eu,Ev])
-    # YUV2RGB matrix
-    yuv2rgb_709_matrix = np.matrix([[1., 0, 1.5747],
-                                    [1., -0.1873, -0.4682],
-                                    [1., 1.8556, 0]])
-    Ergb = np.dot(yuv2rgb_709_matrix, Eyuv)
-    Er = Ergb[0,:].reshape(h,w)
-    Eg = Ergb[1,:].reshape(h,w)
-    Eb = Ergb[2,:].reshape(h,w)
-    #RGB量化   8bit
-    Dr = np.round((219*Er+16)).clip(0,255)
-    Dg = np.round((219*Eg+16)).clip(0.255)
-    Db = np.round((219*Eb+16)).clip(0.255)
-    Drgb = np.array([Dr,Dg,Db]).astype(np.uint8)
-    Drgb = Drgb.transpose(1,2,0)  #(3,1920,1080)->(1920,1080,3)
-    # Drgb = Drgb.astype(np.uint16)
-    return Drgb
+def create_imgs_from_yuvpath(yuv_path, save_path, frames=18, size=(1080,1920), yuv_mode='420'):
+    #读取10bit的YUV
+    Dy_frames, Du_frames, Dv_frames = YUVread_16bit(yuv_path, size=size, frame_num=frames, start_frame=0, mode=yuv_mode)
+    for i in range(frames):
+        #YUV->RGB
+        Drgb = yuv2rgb_2020(Dy_frames[i,:,:], Du_frames[i,:,:], Dv_frames[i,:,:], yuv_bit_depth=10, quantification=16)
+        #保存图像
+        os.makedirs(save_path, exist_ok = True)
+        img_name = os.path.join(save_path, f'frame{i:03d}.png')
+        cv2.imwrite(img_name, cv2.cvtColor(Drgb, cv2.COLOR_RGB2BGR))
+        print(f'已转换第{i}帧')
 
-#有误，待完善
-def yuv2rgb_709(Dy,Du,Dv):
-    """
-    input:Y,U,V
-    output:uint8 Drgb
-    """
-    Dr = Dy + 1.402 * (Dy - 128)
-    print(np.max(Dr))
-    Dg = Dy - 0.34413 * (Du - 128) - 0.71414*(Dv - 128)
-    Db = Dy + 1.772*(Du - 128)
-    Dr = Dr.clip(0.255)
-    Dg = Dg.clip(0.255)
-    Db = Db.clip(0.255)
-    Drgb = np.array([Dr,Dg,Db]).astype(np.uint8)
-    Drgb = Drgb.transpose(1,2,0)  #(3,1920,1080)->(1920,1080,3)
-    # Drgb = Drgb.astype(np.uint16)
-    return Drgb
+def create_imgs_from_yuv(y,u,v,save_path):
+    if len(np.shape(y)) == 3:
+        frame_num = np.shape(y)[0]
+        for i in range(frame_num):
+            #YUV->RGB
+            Drgb = yuv2rgb_2020(y[i,:,:], u[i,:,:], v[i,:,:], yuv_bit_depth=10, quantification=16)
+            #保存图像
+            os.makedirs(save_path, exist_ok = True)
+            img_name = os.path.join(save_path, f'frame{i:03d}.png')
+            cv2.imwrite(img_name, cv2.cvtColor(Drgb, cv2.COLOR_RGB2BGR))
+    else:
+        #YUV->RGB
+        Drgb = yuv2rgb_2020(y, u, v, yuv_bit_depth=10, quantification=16)
+        #保存图像
+        os.makedirs(save_path, exist_ok = True)
+        img_name = os.path.join(save_path, f'frame{i:03d}.png')
+        cv2.imwrite(img_name, cv2.cvtColor(Drgb, cv2.COLOR_RGB2BGR))
 
 def main():
-    yuv_path = 'datasets/HR.yuv'
-    save_path = 'datasets/rgb'
-    #读取前50帧YUV
-    frames = 50
-    #读取10bit的YUV
-    Dy_frames, Du_frames, Dv_frames = YUVread_16bit(yuv_path, (1920,1080), frame_num=frames, start_frame=0, mode='420')
-    #读取8bit的YUV
-    # Dy_frames, Du_frames, Dv_frames = YUVread(yuv_path, (1080,1920), frame_num=frames, start_frame=0, mode='420')
-    for i in range(frames):
-        #U,V分量resize成Y的shape，以便进行矩阵运算
-        # Du = cv2.resize(Du_frames[i,:,:],(Du_frames.shape[2]*2,Du_frames.shape[1]*2),interpolation=cv2.INTER_NEAREST)
-        # Dv = cv2.resize(Dv_frames[i,:,:],(Dv_frames.shape[2]*2,Dv_frames.shape[1]*2),interpolation=cv2.INTER_NEAREST)
-        Du = ndimage.zoom(Du_frames[i,:,:], 2, order=0)
-        Dv = ndimage.zoom(Dv_frames[i,:,:], 2, order=0)
-        #YUV->RGB
-        Drgb = yuv2rgb_2020(Dy_frames[i,:,:], Du, Dv, yuv_bit_depth=10, quantification=8)
-        #Drgb = yuv2rgb_709_matrix(Dy_frames[i,:,:], Du, Dv)
-        cv2.imwrite(os.path.join(save_path, f'frame{i:03d}.png'), cv2.cvtColor(Drgb, cv2.COLOR_RGB2BGR))
-        print(f'已转换第{i}帧')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--yuv_path', type=str, default='G:\datasets\HLG\yuv\gt_from_4k', help='input test image folder')
+    parser.add_argument('--save_path', type=str, default='G:\datasets\HLG\\rgb18\\train_gt', help='save image path')
+    parser.add_argument('--frames', type=int, default=18, help='frames number')
+    parser.add_argument('--size', type=int, default=(1080,1920), help='input video size:(h,w)')
+    args = parser.parse_args()
+    
+    if os.path.isdir(args.yuv_path):
+        yuv_list = sorted(glob.glob(os.path.join(args.yuv_path, '*.*')))
+        for i in range(len(yuv_list)):
+            yuv_name = os.path.splitext(os.path.basename(yuv_list[i]))[0]
+            save_path = os.path.join(args.save_path, yuv_name)
+            os.makedirs(save_path, exist_ok=True)
+            create_imgs_from_yuvpath(yuv_list[i], save_path, frames=args.frames, size=args.size, yuv_mode='420')
+    else: 
+        create_imgs_from_yuvpath(args.yuv_path, args.save_path, frames=args.frames, size=args.size, yuv_mode='420')
 
 if __name__ == '__main__':
     main()
